497da9fe2d0a5fe64c7705646f24669bb7d56fa5
kod0751 <zzdfpi@naver.com>
2025-08-29 00:19:26 +0900

    [feat] zustand counterStore 생성

---
+++ src/app/zustand/stores/counterStore.ts
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+import { CounterState } from '../types/counterType';
+
+export const useCounterStore = create<CounterState>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  reset: () => set({ count: 0 }),
+}));
