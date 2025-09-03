cfcbebd2984707e109fb5e489a346b027c97be16
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-09-03 20:45:21 +0900

    [fix] recoil오류에 따른 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { RecoilRoot } from 'recoil';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import RecoilProvider from './providers/recoil-provider';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -25,10 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <RecoilRoot>{children}</RecoilRoot>
+      <body>
+        <RecoilProvider>{children}</RecoilProvider>
       </body>
     </html>
   );
